f9dfbdae4f2d87e08cff5d819a28d6c989f21621
9240rika_akir0429 <9240rika.akir0429@gmail.com>
2020-02-28 18:37:18 +0900

    version 1.0 Complete

--- src/application/ScreenController.java
+++ src/application/ScreenController.java
@@ -1,5 +1,30 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class ScreenController {
+    @FXML private Button outputButton;
+    @FXML private Label outputLabel;
+    @FXML private TextField inputText;
+    TextField tf = new TextField("");
+    
+    @FXML 
+    protected void onButtonClick(ActionEvent evt) {
+        if(inputText != null && outputButton.getText().equals("表示する")) {
+        outputLabel.setText(inputText.getText());
+        outputButton.setText("clear");
+        }else {
+            outputLabel.setText("");
+            outputButton.setText("表示する");
+        }
+        
+    }
+    
+    
+    
 	
 }
